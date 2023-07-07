fe2d2259a93d89eb9f1b509848f0adee080b4a13
prakharsaki <prakhar2614@gmail.com>
2023-07-07 21:50:37 +0530

    added class inside main class

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,22 +1,25 @@
 package com.driver;
 import java.util.*;
 
-class A {
-    public String meth(){
-        return "Invoking method from class A";
-    }
-}
-class B extends A{
-    @Override
-    public String meth() {
-        return "Method is overridden in Extendend class B";
-    }
-}
-
 public class Main {
+    static class A {
+        public String meth(){
+            return "Invoking method from class A";
+        }
+    }
+    static class B extends A{
+        @Override
+        public String meth() {
+            return "Method is overridden in Extendend class B";
+        }
+    }
+
     public static void main(String[] args) {
         B obj=new B();
         System.out.println(obj.meth());
+
+        B obj1=new B();
+        System.out.println(obj1.meth());
 
     }
 
